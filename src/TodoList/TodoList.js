5660ed8edb70c42d6ad75ed9a53c8cccd43866e5
by09019 <skg09203@naver.com>
2022-01-26 17:06:45 +0900

    * Modified to display the list by TodoList.

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -5,7 +5,7 @@ import {FaCheck} from "react-icons/fa";
 import {ImCross} from "react-icons/im";
 
 const TodoList = () => {
-    const {todoList, changeStateTodo, modifyTodo, deleteTodo} = useContext(InfoContext);
+    const {todoList, viewType, changeStateTodo, modifyTodo, deleteTodo, getTodoIdx} = useContext(InfoContext);
 
     const changeContent = ({key, target: {dataset, value}}) => {
         if (key === "Enter") {
@@ -17,30 +17,32 @@ const TodoList = () => {
         }
     };
 
-    const changeSelected = (itemIdx) => {
-        let item = todoList[itemIdx];
+    const changeSelected = (itemId) => {
+        let item = todoList[getTodoIdx(itemId)];
         item.isSelected = !item.isSelected;
 
         modifyTodo(item);
     };
 
-    console.log("render...");
     return (
         <TodoListStyle>
-            {todoList.map((item, idx) =>
+            {todoList.filter(item => {
+                if(viewType === "all") return true;
+                else return viewType === "activate" ? !item.isSelected : item.isSelected;
+            }).map((item, idx) =>
                 <div key={item.id} className="todo">
                     <div className="input_box">
-                        <input id={"item_" + item.id} type="checkbox" onChange={() => changeSelected(idx)}
+                        <input id={"item_" + item.id} type="checkbox" onChange={() => changeSelected(item.id)}
                                checked={item.isSelected}/>
                         <label htmlFor={"item_" + item.id}><FaCheck/></label>
                     </div>
                     <div className="text_box" onDoubleClick={(e) => changeStateTodo(item.id)}>
                         {item.isModifying ?
-                            <input type="text" className="modifying" defaultValue={item.content} data-idx={idx} onKeyPress={changeContent}/> :
+                            <input type="text" className="modifying" defaultValue={item.content} data-idx={idx} onKeyPress={(e) => changeContent(e)}/> :
                             <p className={item.isSelected ? "selected" : ""}>{item.content}</p>}
                     </div>
                     <div className="button_box">
-                        <button onClick={(e) => deleteTodo(idx)}><ImCross/></button>
+                        <button onClick={(e) => deleteTodo(item.id)}><ImCross/></button>
                     </div>
                 </div>)}
         </TodoListStyle>
